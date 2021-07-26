f22664412280cee4d635f7eace649c83101d92ba
Silas Souza <silasdacosta@gmail.com>
2021-07-26 18:09:24 -0300

    alterando index.php

--- ambiente_dev/bionic/src/index.php
+++ ambiente_dev/bionic/src/index.php
@@ -1,3 +1,19 @@
+# <?php
+# echo "Funcionou!!!"
+# ?>
+
 <?php
-echo "Funcionou!!!"
+echo "Testando conexao <br /> <br />";
+$servername = "192.168.1.24";
+$username = "phpuser";
+$password = "pass";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo "Conectado com sucesso";
 ?>
